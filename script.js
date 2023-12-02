b1bb333fd46c7c5c7adbe493b1417745f32fc637
Sheks21 <152225848+Sheks21@users.noreply.github.com>
2023-12-02 21:02:25 +0100

    portfolio

---
+++ script.js
@@ -0,0 +1,10 @@
+let time = new Date()
+let hour = time.getHours();
+if(hour < 12) {
+   console.log("Good morning")
+}else if (hour < 18) {
+   console.log("Good afternoon")
+}else {
+   console.log("Good evening")
+}
+
